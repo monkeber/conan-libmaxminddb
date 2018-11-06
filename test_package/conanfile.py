ef7a027cbf7e12218cae7f03a50c8c737305641c
e.tiagniriadno <e.tiagniriadno@avantize.com>
2018-11-06 11:42:01 +0200

    Fixed conanfile

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -14,6 +14,11 @@ class MaxminddbTestConan(ConanFile):
         cmake.configure()
         cmake.build()
 
+    def imports(self):
+        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.dylib*", dst="bin", src="lib")
+        self.copy('*.so*', dst='bin', src='lib')
+
     def test(self):
         if not tools.cross_building(self.settings):
             os.chdir("bin")
