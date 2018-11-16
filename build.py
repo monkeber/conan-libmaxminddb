4c661a53d4d6619d1bea9f694be32ddbc5bd87d8
e.tiagniriadno <e.tiagniriadno@avantize.com>
2018-11-16 18:15:57 +0200

    Tried to fix build script

--- build.py
+++ build.py
@@ -3,7 +3,6 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds()
     builder.add(settings={"compiler.libcxx": "libstdc++11",'compiler.version': '4.9',
-        'arch': 'x86', 'build_type': 'Release', 'compiler': 'gcc'})
+        'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'gcc'})
     builder.run()
