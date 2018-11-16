f5c95ba82952d7c01e506e49d05e82f908d37eb1
e.tiagniriadno <e.tiagniriadno@avantize.com>
2018-11-16 18:54:14 +0200

    Added info showing into build script

--- build.py
+++ build.py
@@ -2,6 +2,7 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
+    command = "gcc --version && g++ --version"
     builder = ConanMultiPackager()
     builder.add(settings={'compiler.libcxx': 'libstdc++11', 'arch': 'x86_64',
         'build_type': 'Release', 'compiler': 'gcc'})
