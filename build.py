83a8ff2e62ba7dd412ff101bbe33afa1feb47186
e.tiagniriadno <e.tiagniriadno@avantize.com>
2018-11-16 18:43:56 +0200

    Fixed build

--- build.py
+++ build.py
@@ -3,6 +3,6 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add(settings={"compiler.libcxx": "libstdc++11",'compiler.version': '4.9',
-        'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'gcc'})
+    builder.add(settings={'compiler.libcxx': 'libstdc++11', 'arch': 'x86_64',
+        'build_type': 'Release', 'compiler': 'gcc'})
     builder.run()
