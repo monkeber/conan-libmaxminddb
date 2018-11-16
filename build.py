27c6360bf741b772ed0fc6ded47029a35b9749cf
e.tiagniriadno <e.tiagniriadno@avantize.com>
2018-11-16 18:58:57 +0200

    Added info showing

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     command = "gcc --version && g++ --version"
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(docker_entry_script=command)
     builder.add(settings={'compiler.libcxx': 'libstdc++11', 'arch': 'x86_64',
         'build_type': 'Release', 'compiler': 'gcc'})
     builder.run()
